2fd810f2d7d4c133e2ec87b36cd5fe51d91f16ea
sagarsundr <152969834+sagarsundr@users.noreply.github.com>
2024-02-08 16:04:04 +0530

    User Authentication Service
    UserAuthenticationService

---
+++ userauthenticationservice/src/main/java/com/example/userauthenticationservice/UserAuthenticationControler.java
@@ -0,0 +1,10 @@
+package com.example.userauthenticationservice;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/userauthentication")
+public class UserAuthenticationControler {
+
+}
